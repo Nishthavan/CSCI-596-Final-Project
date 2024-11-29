c1b0aa984438b050cc9bac0c4b2a3454375321e6
Nishthavan <48491021+Nishthavan@users.noreply.github.com>
2024-11-29 20:12:42 +0530

    Updated Shaders.js with Some Util Funcs and Class for Shader Programs

--- src/js/Shaders.js
+++ src/js/Shaders.js
@@ -11,10 +11,9 @@ class ShaderMaterial {
 
     applyKeywords(keywordList) {
         let uniqueHash = keywordList.reduce((accum, keyword) => accum + hashCode(keyword), 0);
-
         let programInstance = this.compiledPrograms[uniqueHash];
         if (!programInstance) {
-            const fragShader = createShader(gl.FRAGMENT_SHADER, this.fragSrc, keywordList);
+            // const fragShader = createShader(gl.FRAGMENT_SHADER, this.fragSrc, keywordList);
             programInstance = generateProgram(this.vertexSrc, fragShader);
             this.compiledPrograms[uniqueHash] = programInstance;
         }
@@ -29,3 +28,35 @@ class ShaderMaterial {
     }
 }
 
+// Shader Program Class
+class ShaderProgram {
+    constructor(vertexSource, fragSource) {
+        this.uniformMap = {};
+        this.programID = generateProgram(vertexSource, fragSource);
+        this.uniformMap = extractUniforms(this.programID);
+    }
+    activate() {
+        gl.useProgram(this.programID);
+    }
+}
+
+// Some Util Funcs...
+function generateProgram(vertexSrc, fragSrc) {
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexSrc);
+    gl.attachShader(shaderProgram, fragSrc);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.trace(gl.getProgramInfoLog(shaderProgram));
+    }
+    return shaderProgram;
+}
+function extractUniforms(programID) {
+    const uniformDetails = [];
+    const activeUniformCount = gl.getProgramParameter(programID, gl.ACTIVE_UNIFORMS);
+    for (let i = 0; i < activeUniformCount; i++) {
+        const uniformInfo = gl.getActiveUniform(programID, i).name;
+        uniformDetails[uniformInfo] = gl.getUniformLocation(programID, uniformInfo);
+    }
+    return uniformDetails;
+}
