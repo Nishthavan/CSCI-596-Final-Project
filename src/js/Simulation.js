6fd2b2dbb28fa51f06687c844f7cdfbce81daca3
Parth Rohilla <parth05rohilla@gmail.com>
2024-11-30 10:25:45 +0530

    Adding initBloomFramebuffers util

--- src/js/Simulation.js
+++ src/js/Simulation.js
@@ -92,3 +92,25 @@ function initFramebuffers () {
     initBloomFramebuffers();
     initSunraysFramebuffers();
 }
+
+function initBloomFramebuffers () {
+    let res = getResolution(config.BLOOM_RESOLUTION);
+
+    const texType = ext.halfFloatTexType;
+    const rgba = ext.formatRGBA;
+    const filtering = ext.supportLinearFiltering ? gl.LINEAR : gl.NEAREST;
+
+    bloom = createFBO(res.width, res.height, rgba.internalFormat, rgba.format, texType, filtering);
+
+    bloomFramebuffers.length = 0;
+    for (let i = 0; i < config.BLOOM_ITERATIONS; i++)
+    {
+        let width = res.width >> (i + 1);
+        let height = res.height >> (i + 1);
+
+        if (width < 2 || height < 2) break;
+
+        let fbo = createFBO(width, height, rgba.internalFormat, rgba.format, texType, filtering);
+        bloomFramebuffers.push(fbo);
+    }
+}
