795fe50825697cc908702c3ac1c236405a8f343d
Rahul Aggarwal <143057753+rahulagg02@users.noreply.github.com>
2024-12-02 19:38:19 -0800

    Adding function curlShader

--- src/js/Controls.js
+++ src/js/Controls.js
@@ -167,3 +167,23 @@ const divergenceShader = compileShader(gl.FRAGMENT_SHADER, `
     }
 `);
 
+const curlShader = compileShader(gl.FRAGMENT_SHADER, `
+    precision mediump float;
+    precision mediump sampler2D;
+
+    varying highp vec2 vUv;
+    varying highp vec2 vL;
+    varying highp vec2 vR;
+    varying highp vec2 vT;
+    varying highp vec2 vB;
+    uniform sampler2D uVelocity;
+
+    void main () {
+        float L = texture2D(uVelocity, vL).y;
+        float R = texture2D(uVelocity, vR).y;
+        float T = texture2D(uVelocity, vT).x;
+        float B = texture2D(uVelocity, vB).x;
+        float vorticity = R - L - T + B;
+        gl_FragColor = vec4(0.5 * vorticity, 0.0, 0.0, 1.0);
+    }
+`);
